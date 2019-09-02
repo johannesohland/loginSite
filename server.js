a9b9ca2a2ae074c5b3e27bddf1cb516b84be3ec4
ssvenlin <sandra.svenlin@gmail.com>
2019-09-02 11:37:49 +0300

    lagt in server.js filen

---
+++ server.js
@@ -0,0 +1,68 @@
+var express = require("express")
+var app = express()
+var db = require("./database.js")
+
+var bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+var HTTP_PORT = 8000
+
+// Start server
+app.listen(HTTP_PORT, () => {
+    console.log("Server running on port %PORT%".replace("%PORT%",HTTP_PORT))
+});
+
+app.use(express.static('public'))
+
+app.get("/loginsite", (req, res, next) => {
+  var sql = "select * from cars"
+  var params = []
+  db.all(sql, params, (err, rows) => {
+      if (err) {
+        res.status(400).json({"error":err.message});
+        return;
+      }
+      res.json({
+          "message":"success",
+          "data":rows
+      })
+    });
+});
+
+
+app.get("/api/cars/make/:make", (req, res, next) => {
+    var sql = "select * from cars where make = ?"
+    var params = [req.params.make]
+    db.all(sql, params, (err, row) => {
+        if (err) {
+          res.status(400).json({"error":err.message});
+          return;
+        }
+        res.json({
+            "message":"success",
+            "data":row
+        })
+      });
+});
+
+
+app.post("loginsite/:id", (req, res, next) => {
+    var sql = "select * from users where id = ?"
+    var params = [req.params.id]
+    db.post(sql, params, (err, row) => {
+        if (err) {
+          res.status(400).json({"error":err.message});
+          return;
+        }
+        res.json({
+            "message":"success",
+            "data":row
+        })
+      });
+});
+
+// Root path
+app.post("/", (req, res, next) => {
+    res.json({"message":"Ok"})
+});
